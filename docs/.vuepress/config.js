d2e648df541c090dc934e961101e2f90619dc9d3
YukiAoi <xxz530990200@gmail.com>
2024-07-19 16:53:07 +0800

    本地缓存

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -118,6 +118,7 @@ export default defineUserConfig({
           "bom",
           "recursion",
           "memory_leak",
+          "cache",
         ],
       },
     ],
---
+++ docs/javascript/cache.md
@@ -0,0 +1,10 @@
+# 本地缓存
+
+## 有哪些
+
+1. cookie
+2. sessionStorage
+3. localStorage
+4. indexedDB
+
+### cookie
